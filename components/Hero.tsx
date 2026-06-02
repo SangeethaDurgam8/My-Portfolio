27300b70c6e06cb250dcc42e4be23efaf2c9711e
SangeethaDurgam8 <durgamsangeetha24@gmail.com>
2026-06-02 06:53:06 +0530

    updated portfolio

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -239,7 +239,7 @@ export default function Hero() {
 
     {/* Portrait */}
     <motion.img
-      src="/sketch-portrait.png"
+      src="public/sketch-portrait.png"
       alt="Sangeetha"
       animate={reduce ? undefined : { y: [0, -10, 0] }}
       transition={{
